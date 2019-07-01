59adebedea894ec898f53dda87b24553bd32c1d3
Cole Rudolph <crudolph@gmail.com>
2019-06-30 22:04:05 -0400

    javascript started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,46 @@ console.log("Cheaters make Baby Jesus cry :'(")
 $("#start").click(function() {
 $("#game-area").remove();
 })
+
+var questions = [{
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},  {
+    question: "",
+    answers: [],
+    correct: ""
+},{
+    question: "",
+    answers: [],
+    correct: ""
+},{
+    question: "",
+    answers: [],
+    correct: ""
+},
+]
