4754b237f143061ee44227ac67ede78e0e3f7189
Cole Rudolph <crudolph@gmail.com>
2019-07-03 17:15:28 -0400

    added assets

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,14 @@ $("#start").click(function() {
 $("#game-area").remove();
 })
 
-var questions = [{
-    question: "",
-    answers: [],
-    correct: ""
+var questionBank = [{
+    question: "Who committed the infamous error in the 1986 World Series?",
+    answers: ["Bill Buckner", "Mookie Wilson", 'Ed "Crazy Legs" McGee', "Oil Can Boyd"],
+    correct: "Bill Buckner"
+},  {
+    question: "In the Simpsons, what was the name of the supervillian who hired Homer briefly?",
+    answers: ["Mr. Burns", "Hank Skorpio", "Lenny", "Professor Frink"],
+    correct: "Hank Skorpio"
 },  {
     question: "",
     answers: [],
@@ -32,7 +36,7 @@ var questions = [{
     question: "",
     answers: [],
     correct: ""
-},  {
+},{
     question: "",
     answers: [],
     correct: ""
@@ -45,4 +49,8 @@ var questions = [{
     answers: [],
     correct: ""
 },
-]
+{
+    question: "What is the name of the song that is currently playing?",
+    answers: ["Every Day I Love You Just a Little Bit More", "Girls Go Crazy", "Wait, there's a song playing?", "Saw Red"],
+    correct: "Saw Red"
+},]
